54db38b564ef5858a1419e7884ff409d9378ff3b
Yupy <yupy_17y@yahoo.com>
2014-11-22 23:13:39 +0200

    Changed Comments order by ASC

--- details.php
+++ details.php
@@ -236,7 +236,7 @@ if ($row["external"] == "yes") {
 print("</table>\n");
 print("<a name='comments' /></a>");
 // comments...
-$subres = $db->query("SELECT comments.id, text, UNIX_TIMESTAMP(added) AS data, user, users.id AS uid FROM comments LEFT JOIN users ON comments.user = users.username WHERE info_hash = '" . $id . "' ORDER BY added DESC");
+$subres = $db->query("SELECT comments.id, text, UNIX_TIMESTAMP(added) AS data, user, users.id AS uid FROM comments LEFT JOIN users ON comments.user = users.username WHERE info_hash = '" . $id . "' ORDER BY added ASC");
 if (!$subres || $subres->num_rows == 0) {
     if (user::$current["uid"] > 1)
         $s = "<br /><br />\n<table width='95%' class='lista'>\n<tr>\n<td align='center'>\n<a href='comment.php?id=" . $id . "&usern=" . urlencode(user::$current["username"]) . "'>" . NEW_COMMENT . "</a>\n</td>\n</tr>\n";

570ef5015ba0662ce90984f5977e7002e9298d82
Yupy <yupy_17y@yahoo.com>
2014-12-02 22:02:34 +0200

    Update redir.php

--- redir.php
+++ redir.php
@@ -1,4 +1,10 @@
 <?php
+/*
+* BtiTracker v1.5.0 is a php tracker system for BitTorrent, easy to setup and configure.
+* This tracker is a frontend for DeHackEd's tracker, aka phpBTTracker (now heavely modified). 
+* Updated and Maintained by Yupy.
+* Copyright (C) 2004-2014 Btiteam.org
+*/
 require_once(dirname(__FILE__).DIRECTORY_SEPARATOR.'include'.DIRECTORY_SEPARATOR.'functions.php');
 
 dbconn();
